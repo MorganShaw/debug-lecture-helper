7216da993b40235de8bc6a073d548ca1619df042
user-1 <user-1@MacBook-Pro.local>
2020-08-20 12:16:53 -0600

    solved

--- src/App.js
+++ src/App.js
@@ -19,15 +19,16 @@ class App extends Component {
   }
 
   getOne() {
-    axios.get(this.state.baseUrl + '/id/1/')
+    axios.get(this.state.baseUrl + '/1')
+    // axios.get(this.state.baseUrl${id}``)
       .then(res => this.setState({ vehicles: res.data }))
   }
   getGreenVehicles() {
     axios.get(this.state.baseUrl + '?color=green')
-      .then(res => this.setState({ vehicles: res.data.vehicles }))
+      .then(res => this.setState({ vehicles: res.data }))
   }
   getFords() {
-    axios.get(this.state.baseUrl + '?make=tesla')
+    axios.get(this.state.baseUrl + '?make=ford')
       .then(res => this.setState({ vehicles: res.data }))
   }
 
@@ -45,7 +46,7 @@ class App extends Component {
           <Button variant='contained' onClick={() => this.getGreenVehicles()}>Green Vehicles</Button>
         </p>
         <p>
-          <Button variant='contained' onClick={() => this.getFords()}>Tesla Vehicles</Button>
+          <Button variant='contained' onClick={() => this.getFords()}>Ford Vehicles</Button>
         </p>
         <hr />
         <h4>Vehicles:</h4>
